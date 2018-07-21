9373975bfb0ef5ef493bac19fdfc173a81ab48ca
JustinSHong <jhong2009@gmail.com>
2018-07-21 16:20:20 -0700

    in median.js: added solution using if/else chain

--- JS_Tasks/median.js
+++ JS_Tasks/median.js
@@ -4,7 +4,25 @@
 // If the array contains an even number of entities, then there is no single middle value, 
 // instead the median becomes the average of the two numbers found in the middle. For this mission, you are given a non-empty array of natural numbers (X).
 //  With it, you must separate the upper half of the numbers from the lower half and find the median.
-
+function median(data) {
+    const sorted = data.sort((a, b) => {
+    	return a - b;
+    });
+    const len = data.length;
+    // determine if the length of the data set is even or odd
+    let median;
+    // if len is odd: median is at index Math.floor(len / 2)
+    if (len % 2 === 1) {
+        median = data[Math.floor(len / 2)];
+    }
+    // if len is even: find average of nums at indices (len / 2) and (len / 2) - 1
+    else {
+        const upper = data[len / 2];
+        const lower = data[(len / 2) - 1];
+        median = (upper + lower) / 2;
+    }
+    return median;
+}
 // Test Cases:
 median([1, 2, 3, 4, 5]) // 3
 median([3, 1, 2, 5, 3]) // 2
