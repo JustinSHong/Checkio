88a488fe9cc4a85850e775cf82c7abcb8fecca1d
JustinSHong <jhong2009@gmail.com>
2018-07-22 17:58:38 -0700

    in countNeighbors.js: reformatted task description to be more readable

--- JS_Tasks/countNeighbors.js
+++ JS_Tasks/countNeighbors.js
@@ -1,7 +1,13 @@
 // Task Description:
-// In cellular automata, the Moore neighborhood comprises the eight cells surrounding a central cell on a two-dimensional square lattice. The neighborhood is named after Edward F. Moore, a pioneer of cellular automata theory. Many board games are played with a rectangular grid with squares as cells. For some games, it is important to know about the conditions of neighbouring cells for chip (figure, draught etc) placement and strategy.
+// In cellular automata, the Moore neighborhood comprises the eight cells surrounding a central cell on a two-dimensional square lattice.
+// The neighborhood is named after Edward F. Moore, a pioneer of cellular automata theory.
+// Many board games are played with a rectangular grid with squares as cells.
+// For some games, it is important to know about the conditions of neighbouring cells for chip (figure, draught etc) placement and strategy.
 
-// You are given a state for a rectangular board game grid with chips in a binary matrix, where 1 is a cell with a chip and 0 is an empty cell. You are also given the coordinates for a cell in the form of row and column numbers (starting from 0). You should determine how many chips are close to this cell. Every cell interacts with its eight neighbours; those cells that are horizontally, vertically, or diagonally adjacent.
+// You are given a state for a rectangular board game grid with chips in a binary matrix, where 1 is a cell with a chip and 0 is an empty cell.
+// You are also given the coordinates for a cell in the form of row and column numbers (starting from 0).
+// You should determine how many chips are close to this cell. Every cell interacts with its eight neighbours;
+// those cells that are horizontally, vertically, or diagonally adjacent.
 
 function countNeighbours(data, row, col) {
 	let neighborCount = 0; // number of neighbors of the given cell
