916d17e514a3d9265a8ba516245fb195f87c1de4
JustinSHong <jhong2009@gmail.com>
2018-07-22 17:59:58 -0700

    in nonUniqueElements.js: set u pinintial solution

--- JS_Tasks/nonUniqueElements.js
+++ JS_Tasks/nonUniqueElements.js
@@ -5,6 +5,20 @@
 // When solving this task, do not change the order of the list.
 // Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
 
+function nonUniqueElements(data) {
+	const result = []; // holds all non-unique ints
+	for (let i = 0; i < data.length; i++) {
+		let el = data[i]; // element to be tested for uniqueness
+		for (let j = 0; j < data.length; j++) {
+			if (j !== i && el === data[j]) {
+				result.push(el);
+				break; // once an element is determined to be unique test the next one
+			}
+		}
+	}
+	return result;
+}
+
 // Test Cases:
 nonUniqueElements([1, 2, 3, 1, 3]); // [1, 3, 1, 3]
 nonUniqueElements([1, 2, 3, 4, 5]); // []
