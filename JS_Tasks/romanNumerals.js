96669db5efc131c9c0caf6c3703796a063338960
JustinSHong <jhong2009@gmail.com>
2018-07-23 22:48:59 -0700

    in romanNumerals.js: set up task description and test cases

---
+++ JS_Tasks/romanNumerals.js
@@ -0,0 +1,20 @@
+// Task Description:
+// Roman numerals come from the ancient Roman numbering system.
+// They are based on specific letters of the alphabet which are combined to signify the sum (or, in some cases, the difference) of their values.
+// The first ten Roman numerals are:
+
+// I, II, III, IV, V, VI, VII, VIII, IX, and X.
+
+// I	1 (unus)
+// V	5 (quinque)
+// X	10 (decem)
+// L	50 (quinquaginta)
+// C	100 (centum)
+// D	500 (quingenti)
+// M	1,000 (mille)
+
+// Test Cases:
+romanNumerals(6); // 'VI'
+romanNumerals(76); // 'LXXVI'
+romanNumerals(499); // 'CDXCIX'
+romanNumerals(3888); //'MMMDCCCLXXXVIII'
