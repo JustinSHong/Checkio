74524c9c112f97bc41edf057b9b570f16d76d2ea
JustinSHong <jhong2009@gmail.com>
2018-07-24 21:19:45 -0700

    nonUniqueElements.py: added solution using Counter datatype

--- Python_Tasks/nonUniqueElements.py
+++ Python_Tasks/nonUniqueElements.py
@@ -5,6 +5,13 @@
 # When solving this task, do not change the order of the list.
 # Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
 
+# Solution 2
+def nonUniqueElements(data):
+	# creates an unordered collection where els are stored as dicts and their counts are stored as values
+	counter = Counter(data)
+	return [x for x in data if counter[x] > 1]
+
+# Solution 1
 def nonUniqueElements(data):
 	counter = {};
 	for num in data:
