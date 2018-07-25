5d9320817cc13e1134cca0b2f7e93a7efe3ef802
JustinSHong <jhong2009@gmail.com>
2018-07-24 20:48:33 -0700

    nonUniqueElements.py: set up task description and test cases

---
+++ Python_Tasks/nonUniqueElements.py
@@ -0,0 +1,12 @@
+# Task Description:
+# You are given a non-empty list of integers (X).
+# For this task, you should return a list consisting of only the non-unique elements in this list.
+# To do so you will need to remove all unique elements (elements which are contained in a given list only once).
+# When solving this task, do not change the order of the list.
+# Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
+
+# Test Cases:
+nonUniqueElements([1, 2, 3, 1, 3]); // [1, 3, 1, 3]
+nonUniqueElements([1, 2, 3, 4, 5]); // []
+nonUniqueElements([5, 5, 5, 5, 5]); // [5, 5, 5, 5, 5]
+nonUniqueElements([10, 9, 10, 10, 9, 8]); // [10, 9, 10, 10, 9]
