4d4d6c81b97507718433771cc9cf60a77cdaa36f
JustinSHong <jhong2009@gmail.com>
2018-07-25 21:01:07 -0700

    sayHi.js: set up task description and test cases

---
+++ JS_Tasks/sayHi.js
@@ -0,0 +1,8 @@
+// Task Description:
+// In this mission you should write a function that introduce a person with a given parameters in attributes.
+// Input: Two arguments. String and positive integer.
+// Output: String.
+
+// Test Cases:
+sayHi("Alex", 32); // "Hi. My name is Alex and I'm 32 years old"
+sayHi("Frank", 68); // "Hi. My name is Frank and I'm 68 years old"	
