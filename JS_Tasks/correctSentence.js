c79a01c38346bc27b477d953d998f469b3137aaf
JustinSHong <jhong2009@gmail.com>
2018-07-25 21:15:05 -0700

    correctSentence.js: added solution using ternary operator

--- JS_Tasks/correctSentence.js
+++ JS_Tasks/correctSentence.js
@@ -4,6 +4,16 @@
 // Pay attention to the fact that not all of the fixes is necessary.
 // If a sentence already ends with a dot then adding another one will be a mistake.
 
+function correctSentence(str) {
+	// check if str ends with '.'
+	str[str.length - 1] === "." ? null : (str += ".");
+	// check if str begins with a capital letter
+	str[0] === str.toUpperCase()
+		? null
+		: (str = str[0].toUpperCase() + str.slice(1));
+	return str;
+}
+
 // Test Cases:
 correctSentence("greetings, friends"); // "Greetings, friends."
 correctSentence("Greetings, friends"); // "Greetings, friends."
