de5778ccb7a9fe25fc181bca890f9a6a466210b5
JustinSHong <jhong2009@gmail.com>
2018-07-25 21:06:16 -0700

    correctSentence.js: set up task description and test cases

---
+++ JS_Tasks/correctSentence.js
@@ -0,0 +1,10 @@
+// Task Description:
+// For the input of your function will be given one sentence.
+// You have to return its fixed copy in a way so it’s always starts with a capital letter and ends with a dot.
+// Pay attention to the fact that not all of the fixes is necessary.
+// If a sentence already ends with a dot then adding another one will be a mistake.
+
+// Test Cases:
+correctSentence("greetings, friends"); // "Greetings, friends."
+correctSentence("Greetings, friends"); // "Greetings, friends."
+correctSentence("Greetings, friends."); // "Greetings, friends."
