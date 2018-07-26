6052d3d2bd7022291527fe5cf6336a61d9a53cf6
JustinSHong <jhong2009@gmail.com>
2018-07-25 21:14:22 -0700

    firstWord.js: set up task description and test cases

---
+++ JS_Tasks/firstWord.js
@@ -0,0 +1,11 @@
+// Task Description:
+// You are given a string where you have to find its first word.
+// When solving a task pay attention to the following points:
+// There can be dots and commas in a string.
+// A string can start with a letter or, for example, a dot or space.
+// A word can contain an apostrophe and it's a part of a word.
+// The whole text can be represented with one word and that's it.
+
+// Test Cases:
+firstWord("Hello world"); // "Hello"
+firstWord("greetings, friends"); // "greetings"
