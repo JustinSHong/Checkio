0419500a68784636944948f42515fc28e844c718
JustinSHong <jhong2009@gmail.com>
2018-07-25 22:02:49 -0700

    firstWord.js: added solution using regex

--- JS_Tasks/firstWord.js
+++ JS_Tasks/firstWord.js
@@ -6,6 +6,27 @@
 // A word can contain an apostrophe and it's a part of a word.
 // The whole text can be represented with one word and that's it.
 
+function firstWord(str) {
+	// parse initial string
+	const regex = /[^'a-zA-Z']/g; // any non-apostrophe or non-letter chars
+	const regex2 = /['a-zA-Z']/g;
+	const words = str.trim().split(regex);
+	let first;
+
+	for (let w of words) {
+		if (!regex.test(w) && w !== "") {
+			first = w;
+			break;
+		}
+	}
+	return first;
+}
+
 // Test Cases:
-firstWord("Hello world"); // "Hello"
-firstWord("greetings, friends"); // "greetings"
+firstWord("Hello.World");
+firstWord("Hello world"); // "Hello")
+firstWord(" a word "); // "a")
+firstWord("don't touch it"); // "don't")
+firstWord("greetings, friends"); // "greetings")
+firstWord("... and so on ..."); // "and")
+firstWord("hi"); // "hi")
