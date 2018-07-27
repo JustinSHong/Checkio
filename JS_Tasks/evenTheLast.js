2b34dc9afc89d441bff2e463bf7ad8dc58573326
JustinSHong <jhong2009@gmail.com>
2018-07-26 21:39:49 -0700

    evenTheLast.js: added initial solution

--- JS_Tasks/evenTheLast.js
+++ JS_Tasks/evenTheLast.js
@@ -5,6 +5,17 @@
 
 // For an empty array, the result will always be 0 (zero).
 
+function evenLast(arr) {
+	if (arr.length === 0) return 0;
+	let sum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (i % 2 === 0) sum += arr[i];
+	}
+
+	return sum * arr[arr.length - 1];
+}
+
 // Test Cases:
 evenLast([0, 1, 2, 3, 4, 5]); // 30
 evenLast([1, 3, 5]); // 30
