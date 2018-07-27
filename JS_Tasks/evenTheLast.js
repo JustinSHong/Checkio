a2cafa929b62e125f9f1cfc50e1597c78e5b0b06
JustinSHong <jhong2009@gmail.com>
2018-07-26 21:31:55 -0700

    evenTheLast.js: set up task description and test cases

---
+++ JS_Tasks/evenTheLast.js
@@ -0,0 +1,12 @@
+// Task Description:
+// You are given an array of integers. You should find the sum of the elements with even indexes (0th, 2nd, 4th...)
+// then multiply this summed number and the final element of the array together.
+// Don't forget that the first element has an index of 0.
+
+// For an empty array, the result will always be 0 (zero).
+
+// Test Cases:
+evenLast([0, 1, 2, 3, 4, 5]); // 30
+evenLast([1, 3, 5]); // 30
+evenLast([6]); // 36
+evenLast([]); // 0
