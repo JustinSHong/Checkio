e0ebb87bfbb1581e76cba82ab299ab3a76d99d52
JustinSHong <jhong2009@gmail.com>
2018-07-27 19:43:05 -0700

    in digitsMultiplication.js: set up task description and test cases

---
+++ JS_Tasks/digitsMultiplication.js
@@ -0,0 +1,11 @@
+// Task Description:
+// You are given a positive integer.
+// Your function should calculate the product of the digits excluding any zeroes.
+// For example: The number given is 123405.
+// The result will be 1*2*3*4*5=120 (don't forget to exclude zeroes).
+
+// Test Cases:
+digitsMultip(123405); // 120
+digitsMultip(999); // 729
+digitsMultip(1000); // 1
+digitsMultip(1111); // 1
