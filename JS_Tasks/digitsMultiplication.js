47a22e68ca2b8544fb404664521aa98cdfc1bd9f
JustinSHong <jhong2009@gmail.com>
2018-07-27 19:50:31 -0700

    digitsMultiplication.js: added initial solution

--- JS_Tasks/digitsMultiplication.js
+++ JS_Tasks/digitsMultiplication.js
@@ -4,6 +4,20 @@
 // For example: The number given is 123405.
 // The result will be 1*2*3*4*5=120 (don't forget to exclude zeroes).
 
+function digitsMultip(n) {
+	let str = n.toString().split("");
+	str = str.filter(char => {
+		return char !== "0"; // exclude all 0s
+	});
+
+	let product = 1;
+	str.forEach(char => {
+		let int = Number(char);
+		product *= char;
+	});
+	return product;
+}
+
 // Test Cases:
 digitsMultip(123405); // 120
 digitsMultip(999); // 729
