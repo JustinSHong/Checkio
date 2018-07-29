74cba118557d754790781e9cc1272b109f7c3a8e
JustinSHong <jhong2009@gmail.com>
2018-07-28 23:11:10 -0700

    commondwords.js: set up initial solution

--- JS_Tasks/commonWords.js
+++ JS_Tasks/commonWords.js
@@ -8,7 +8,19 @@
 // Input: Two arguments as strings.
 // Output: The common words as a string.
 
+function commonWords(str1, str2) {
+	const words1 = str1.split(",");
+	const words2 = str2.split(",");
+
+	let result = [];
+	for (let word of words1) {
+		if (words2.includes(word)) result.push(word);
+	}
+
+	return result.sort().join(",");
+}
+
 // Test Cases:
-commonWords("hello,world", "hello,earth"); // "hello", "Hello"
-commonWords("one,two,three", "four,five,six"); // "", "Too different"
-commonWords("one,two,three", "four,five,one,two,six,three"); // "one,three,two", "1 2 3"
+commonWords("hello,world", "hello,earth"); // "hello"
+commonWords("one,two,three", "four,five,six"); // ""
+commonWords("one,two,three", "four,five,one,two,six,three"); // "one,three,two"
