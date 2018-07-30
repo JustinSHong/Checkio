5cadac65a9e9c10b556ccb00ba0c0761a01a5667
JustinSHong <jhong2009@gmail.com>
2018-07-29 22:59:18 -0700

    theMostFrequent.js: set up initial solution

--- JS_Tasks/theMostFrequent.js
+++ JS_Tasks/theMostFrequent.js
@@ -1,6 +1,17 @@
 // Task Description:
 // You have a sequence of strings, and you’d like to determine the most frequently occurring string in the sequence.
 
+function mostFrequent(arr) {
+	const count = {}; // holds frequencies of all strings in arr
+	for (let str of arr) {
+		count.hasOwnProperty(str) ? count[str]++ : (count[str] = 0);
+	}
+	let maxFreq = Math.max(...Object.values(count));
+	for (let key of Object.keys(count)) {
+		if (count[key] === maxFreq) return key;
+	}
+}
+
 // Test Cases:
 mostFrequent(["a", "b", "c", "a", "b", "a"]); // "a"
 mostFrequent(["a", "b", "c", "a", "b", "a"]); // "a"
