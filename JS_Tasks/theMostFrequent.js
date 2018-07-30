190a1d475cd650d5ced15d644e5e2029be9ad5f1
JustinSHong <jhong2009@gmail.com>
2018-07-29 22:40:34 -0700

    theMostFrequent.js: set up task description and test cases

---
+++ JS_Tasks/theMostFrequent.js
@@ -0,0 +1,7 @@
+// Task Description:
+// You have a sequence of strings, and you’d like to determine the most frequently occurring string in the sequence.
+
+// Test Cases:
+mostFrequent(["a", "b", "c", "a", "b", "a"]); // "a"
+mostFrequent(["a", "b", "c", "a", "b", "a"]); // "a"
+mostFrequent(["a", "a", "bi", "bi", "bi"]); // "bi"
