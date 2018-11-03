1e2164d9008896f44bd7e5442434ddb967d2e062
JustinSHong <jhong2009@gmail.com>
2018-11-02 18:32:41 -0700

    theShipTeams.js: uploaded initial solution

--- JS_Tasks/theShipTeams.js
+++ JS_Tasks/theShipTeams.js
@@ -5,6 +5,21 @@
 // The input data will be the dictionary where keys will be the surnames of the sailors and the values will be their ages.
 // After the crew formating, you should sort all of the sailors in the alphabetical order in the each list of surname
 
+function twoTeams(sailors) {
+    const team1 = []; // holds sailors younger than 20 and older than 40
+    const team2 = []; // holds sailors between the ages 20 and 40
+    const sailorNames = Object.keys(sailors);
+
+    sailorNames.forEach(sailor => {
+        sailors[sailor] < 20 || sailors[sailor] > 40
+            ? team1.push(sailor)
+            : team2.push(sailor);
+    });
+
+    const sorted = [team1.sort(), team2.sort()];
+    return sorted;
+}
+
 // Test Cases
 twoTeams({
     Smith: 34,
