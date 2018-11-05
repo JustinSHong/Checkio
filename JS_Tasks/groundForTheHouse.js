7e7197c0bdba81cb49816569c2b6e9c43411dea1
JustinSHong <jhong2009@gmail.com>
2018-11-04 17:28:36 -0800

    groundForTheHouse.js: added solution

--- JS_Tasks/groundForTheHouse.js
+++ JS_Tasks/groundForTheHouse.js
@@ -6,6 +6,33 @@
 // where '0' means the empty piece of the ground and the '#' is the piece of your house.
 // Your task is to count the minimal area of the rectangle ground which is enough for the building.
 
+function house(plan) {
+    function clip(arr) {
+        // trimming from start of the plan array
+        while (arr.length && !arr[0].includes("#")) {
+            arr.shift();
+        }
+        // trimmuing from end of the plan array
+        while (arr.length && !arr[arr.length - 1].includes("#")) {
+            arr.pop();
+        }
+        return arr;
+    }
+
+    if (!plan.includes("#")) return 0; // no plots for house exist
+    // otherwise trim the plan
+    plan = clip(
+        plan
+            .trim()
+            .split("\n")
+            .map(r => [...r])
+    ); // parse and clip
+    console.log(`plan parsed and clipped ${plan}`);
+    plan = clip(plan[0].map((_, c) => plan.map(r => r[c]))); // transpose and clip
+    console.log(`plan transposed and clipped ${plan}`);
+    return plan.length * plan[0].length;
+}
+
 // Test Cases:
 house(`
 0000000
