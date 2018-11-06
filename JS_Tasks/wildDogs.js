e669b768b2fba2815b85abdf9b101db9d2b14e22
JustinSHong <jhong2009@gmail.com>
2018-11-05 23:25:23 -0800

    wildDogs.js: added task description and test cases

---
+++ JS_Tasks/wildDogs.js
@@ -0,0 +1,14 @@
+// Task Description
+// As input you’ll be given the coordinates of the dogs.
+// Your task is to find the distance to the nearest point
+// from which you can kill the maximum number of animals with one shot
+// (any number of dogs on the same line can be killed with one shot).
+// Your starting position is the point (0, 0).
+// If the calculated distance is an integer, return it as int, otherwise round it to 2 decimal places.
+
+// Test Cases:
+wildDogs([[7, 122], [8, 139], [9, 156], [10, 173], [11, 190], [-100, 1]]); // 0.18
+
+wildDogs([[6, -0.5], [3, -5], [1, -20]]); // 3.63
+
+wildDogs([[10, 10], [13, 13], [21, 18]]); // 0
