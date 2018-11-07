9ad9c66b8025d5d099f5284dc9e30e2e5e89cf34
JustinSHong <jhong2009@gmail.com>
2018-11-07 15:18:46 -0800

    brakets.js: uploaded task description and test cases

---
+++ JS_Tasks/brackets.js
@@ -0,0 +1,19 @@
+// Task Description
+// You are given an expression with numbers, brackets and operators.
+// For this task only the brackets matter. Brackets come in three flavors: "{}" "()" or "[]".
+// Brackets are used to determine scope or to restrict some expression.
+// If a bracket is open, then it must be closed with a closing bracket of the same type.
+// The scope of a bracket must not intersected by another bracket.
+// In this task you should make a decision, whether to correct an expression or not based on the brackets.
+// Do not worry about operators and operands.
+
+// Input: An expression with different of types brackets as a string (unicode).
+// Output: A verdict on the correctness of the expression in boolean (True or False)
+
+// Test Cases:
+brackets("((5+3)*2+1)"); // true
+brackets("{[(3+1)+2]+}"); // true
+brackets("(3+{1-1)}"); // false
+brackets("[1+1]+(2*2)-{3/3}"); // true,
+brackets("(({[(((1)-2)+3)-3]/3}-3)"); // false
+brackets("2+3"); // true
