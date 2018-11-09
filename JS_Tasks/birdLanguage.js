fecc406ec824ab700eaea16fc4a1ec3433e30578
JustinSHong <jhong2009@gmail.com>
2018-11-08 17:38:49 -0800

    birdLanguage.js: added task description

---
+++ JS_Tasks/birdLanguage.js
@@ -0,0 +1,13 @@
+// Task Description
+// You are given an ornithological phrase as several words which are separated by white-spaces (each pair of words by one whitespace).
+// The bird does not know how to punctuate its phrases and only speaks words as letters. All words are given in lowercase.
+// You should translate this phrase from the bird language to something more understandable.
+
+// Input: A bird phrase as a string.
+// Output: The translation as a string.
+
+// Test Cases:
+translate("hieeelalaooo"); //"hello"
+translate("hoooowe yyyooouuu duoooiiine"); // "how you doin"
+translate("aaa bo cy da eee fe"); // "a b c d e f"
+translate("sooooso aaaaaaaaa"); // "sos aaa"
