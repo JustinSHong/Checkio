aea6d39d6d87f5ec33d792f1f7663489084c2c7d
JustinSHong <jhong2009@gmail.com>
2018-11-09 22:16:15 -0800

    superRoot.js: set up task descriptioni and test cases

---
+++ JS_Tasks/superRoot.js
@@ -0,0 +1,14 @@
+// Task Description
+// Square roots, cube roots, 4th roots... each one of them is too boring for Nicola.
+// He needs to find the super root! With your help he'll almost certainly achieve this goal.
+
+// The super root of a number N is the number x, such that xx = N.
+// The result should be accurate, so that x**x ≈ N±0.001, or N - 0.001 < x**x < N + 0.001.
+
+// Input: A number (N) as an integer.
+// Output: The super root (x) as a float or an integer.
+
+// Test Cases:
+superRoot(4); // 2;
+superRoot(27); // 3;
+superRoot(81); // 3.504339593597054;
