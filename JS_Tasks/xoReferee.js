cc1b0ff0dec82bc13620f8be372451c569ac88d2
JustinSHong <jhong2009@gmail.com>
2018-11-09 18:25:20 -0800

    xoReferee.js: added initial solution

--- JS_Tasks/xoReferee.js
+++ JS_Tasks/xoReferee.js
@@ -8,6 +8,44 @@
 // Input: A game result as a list of strings (unicode).
 // Output: "X", "O" or "D" as a string.
 
+function xoReferee(board) {
+    let col1 = "",
+        col2 = "",
+        col3 = "",
+        diagLR = "",
+        diagRL = "";
+
+    for (let i = 0; i < board.length; i++) {
+        const row = board[i];
+        if (row == "XXX") return "X";
+        else if (row === "OOO") return "O";
+        col1 += row[0];
+        col2 += row[1];
+        col3 += row[2];
+        diagLR += row[i];
+        diagRL += row[2 - i];
+    }
+    if (
+        col1 == "XXX" ||
+        col2 == "XXX" ||
+        col3 == "XXX" ||
+        diagLR == "XXX" ||
+        diagRL == "XXX"
+    )
+        return "X";
+    else if (
+        col1 == "OOO" ||
+        col2 == "OOO" ||
+        col3 == "OOO" ||
+        diagLR == "OOO" ||
+        diagRL == "OOO"
+    )
+        return "O";
+    else {
+        return "D";
+    }
+}
+
 // Test Cases:
 xoReferee(["X.O", "XX.", "XOO"]); // "X"
 xoReferee(["OO.", "XOX", "XOX"]); // "O"
