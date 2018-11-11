6a41873b691665d49af12d68b35c5d5da8747072
JustinSHong <jhong2009@gmail.com>
2018-11-10 16:55:06 -0800

    simpleAreas.js: added initial solution using switch()

--- JS_Tasks/simpleAreas.js
+++ JS_Tasks/simpleAreas.js
@@ -13,6 +13,32 @@
 // Input: One, two or three arguments as floats or as integers.
 // Output: The area of the circle, rectangle or triangle as a float.
 
+function simpleAreas(ints) {
+    var args = Array.from(arguments);
+    switch (args.length) {
+        case 1:
+            // find area of a circle
+            const areaOfCircle = Math.PI * Math.pow(args / 2, 2);
+            return parseFloat(areaOfCircle.toFixed(2), 10);
+        case 2:
+            // find area of a rectangle
+            const areaOfRect = args[0] * args[1];
+            return areaOfRect;
+        case 3:
+            // find area of a triangle with Hero's Formula
+            const perimeter = (args[0] + args[1] + args[2]) / 2;
+            const areaOfTriangle = Math.sqrt(
+                perimeter *
+                    (perimeter - args[0]) *
+                    (perimeter - args[1]) *
+                    (perimeter - args[2])
+            );
+            return parseFloat(areaOfTriangle.toFixed(2), 10);
+        default:
+            return 0;
+    }
+}
+
 // Test Cases:
 simpleAreas(3); // 7.07
 simpleAreas(2, 2); // 4
